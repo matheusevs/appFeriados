e879e58cafc97f585d1a0ddcd5721d75cf3e6f73
Matheus Eduardo <matheus.eduardo@versatecnologia.com.br>
2024-03-21 22:04:57 -0300

    feat: Aula do dia 21/03

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  public feriados: any = []; 
+
+  constructor() { // Será executado toda vez que a página for carregada
+    this.criarFeriados();
+  }
+
+  //Função responsável por criar os feriados dentro da variavel this.feriados
+  criarFeriados() {
+
+    this.feriados = [
+      {nome: 'Rafael'},
+      {nome: 'Vitoria'},
+      {nome: 'Andre'},
+      {nome: 'Luciano cruzeirense'}
+    ];
+
+    console.log(this.feriados);
+
+  }
 
 }
